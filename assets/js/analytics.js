a236a607648d9d11ea719d8eac2bc8b723a0b3db
Arceniy Khramtsov <Dubsta.post@yandex.ru>
2020-01-21 00:53:00 +0300

    Analytics page: bug fix of showing charts, showing currend period type. created select dropdown

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -7,9 +7,9 @@ function bindAnalytics() {
     body.append(loader);
 
 
-    renderSummatyBlock('traffic');
-    setTimeout(() => { renderSummatyBlock('impression'); }, 1000)
-    setTimeout(() => { renderSummatyBlock('gender'); }, 2000)
+    renderSummaryBlock('traffic');
+    setTimeout(() => { renderSummaryBlock('impression'); }, 1000)
+    setTimeout(() => { renderSummaryBlock('gender'); }, 2000)
 
     renderLargeBlock('traffic');
     renderLargeBlock('impression');
@@ -27,32 +27,74 @@ function bindAnalytics() {
 
     $('#analytics-page .chart-selectors .selector').click(function (e) {
         if (e.target.closest('.selector').classList[2] != 'active')
-            renderSummatyBlock(e.target.closest('.selector').classList[1].split('-')[0]);
+            renderSummaryBlock(e.target.closest('.selector').classList[1].split('-')[0]);
     });
 }
-function AnalyticsInit() { }
+function AnalyticsInit() {    
+    $('#analytics-page .card:first-child .chart-block .info').append(
+`<div class = 'dropdown period-dropdown'>
+    <div class = "visible-dropdown">
+        <h6>Week</h6>
+        <div class = "toggle-dropdown">
+            <i class="fa fa-clock" aria-hidden="true"></i>
+            <i class="fa fa-caret-down" aria-hidden="true"></i>
+        </div>
+    </div>
+    <div class="dropdown-list">
+        <h6>Select timespan</h6>
+        <div class = "dropdown-element">Year</div>
+        <div class = "dropdown-element">Month</div>
+        <div class = "dropdown-element">Week</div>
+        <div class = "dropdown-element">Day</div>
+    </div>
+</div >`);
+
+    var dropdownBtn = $('#analytics-page .card:first-child .chart-block .dropdown .toggle-dropdown');
+    var dropdownList = $('#analytics-page .card:first-child .chart-block .dropdown .dropdown-list');
+    
+
+    dropdownBtn.click(function () {
+        dropdownList[0].classList.toggle('active');
+    })
+
+    
+    dropdownList.click(function (e) {
+        console.log(e)
+        if (e.target.className == 'dropdown-element') {
+            $('#analytics-page .card:first-child .chart-block .visible-dropdown h6')[0].textContent = e.target.textContent;
+            dropdownList[0].classList.toggle('active');
+        }
+    });
+}
 
 
 
-function renderSummatyBlock(entriesType) {
+function renderSummaryBlock(entriesType) {
     togglePageSelector(entriesType);
     $.when(getMonitors()).then(function (r1) {
         var monitors = r1.data;
+        var period = 'year';
 
-        var start = 1578862800000;
-        var end = 1579467599999;
-        var period = 'day';
+        var timestampObject = getAnalyticPagePeriod(period);
+        var start = timestampObject.start1;
+        var end = timestampObject.end1;
+        var newPeriod = timestampObject.newPeriod;
+        var oldPeriod = period;
+
         var counter = 0;
         var maxLength = 0;
         var chartDataObject = [];
+
+        var queries = [];
         for (monitor of monitors) {
-            var query = `monitor_id=${monitor.id}&start=${start}&end=${end}&period=${period}`;
+            queries.push(`monitor_id=${monitor.id}&start=${start}&end=${end}&period=${newPeriod}`)
+        }
 
-            $.when(
-                getDataOfMonitor(entriesType, query)
-            ).done(function (r) {
-                var result = r.data;
-
+        $.when(
+            ...getRequestsArr(entriesType, queries)
+        ).done(function (...results) {
+            for (r of results) {
+                var result = r[0].data;
                 if (result.length > maxLength)
                     maxLength = result.length;
 
@@ -89,15 +131,13 @@ function renderSummatyBlock(entriesType) {
                 });
 
                 if (monitors[counter].id == monitors[monitors.length - 1].id) {
-                    chartDataObject['labels'] = getLabels(period, maxLength);
+                    chartDataObject['labels'] = getLabels(newPeriod, maxLength);
                     renderSummaryGend(chartDataObject, entriesType);
-                    showAnalyticsSummaryStats(chartDataObject, period);
+                    showAnalyticsSummaryStats(chartDataObject, oldPeriod);
                 }
                 counter++;
-            });
-
-
-        }
+            }
+        });
     });
 }
 
@@ -200,7 +240,7 @@ function renderLargeBlock(entriesType) {
     var id = "16f4afce-2c95-4e05-b907-5fc386b8d373";
     $.when(getMonitor(id)).then(function (r1) {
         var monitor = r1.data;
-        var period = 'day';
+        var period = 'week';
         var maxLength = 0;
         var chartDataObject = [];
         var card = $('#analytics-page .large-' + entriesType + '  .info');
@@ -211,68 +251,23 @@ function renderLargeBlock(entriesType) {
             "</div > "
         );
 
-        var start1 = 0;
-        var end1 = 0;
-        var start2 = 0;
-        var end2 = 0;
-        var cur = moment();
+        var timestampObject = getAnalyticPagePeriod(period);
+
+        var start1 = timestampObject.start1;
+        var end1 = timestampObject.end1;
+        var start2 = timestampObject.start2;
+        var end2 = timestampObject.end2;
+        var newPeriod = timestampObject.newPeriod;
         var oldPeriod = period;
-        var newPeriod = '';
-
-        switch (period) {
-            case 'day':
-                start1 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'start');
-                end1 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'end');
-                cur.subtract(1, period);
-
-                start2 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'start');
-                end2 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'end');
-                newPeriod = 'hour';
-                break;
-            case 'week':
-                end1 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'end');
-                cur.subtract(1, period);
-                start1 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'start');
-
-                end2 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'end');
-                cur.subtract(1, period);
-                start2 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'start');
-                newPeriod = 'day';
-                break;
-            case 'month':
-                end1 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'end');
-                cur.subtract(1, period);
-                start1 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'start');
-
-                end2 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'end');
-                cur.subtract(1, period);
-                start2 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'start');
-                newPeriod = 'week';
-                break;
-            case 'year':
-                end1 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'end');
-                cur.subtract(1, period);
-                start1 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'start');
-
-                end2 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'end');
-                cur.subtract(1, period);
-                start2 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'start');
-                newPeriod = 'month';
-                break;
-            default:
-                break;
-        }
 
         var queries = [
             `monitor_id=${monitor.id}&start=${start1}&end=${end1}&period=${newPeriod}`,
             `monitor_id=${monitor.id}&start=${start2}&end=${end2}&period=${newPeriod}`,
         ];
-        for (query of queries) {
-            $.when(
-                getDataOfMonitor(entriesType, query)
-            ).done(function (r) {
-                console.log(r)
-                var result = r.data;
+
+        $.when(...getRequestsArr(entriesType, queries)).done(function (...results) {
+            for (r of results) {
+                var result = r[0].data;
 
                 if (result.length > maxLength)
                     maxLength = result.length;
@@ -298,7 +293,6 @@ function renderLargeBlock(entriesType) {
                     }
                 }
 
-
                 chartDataObject.push({
                     id: monitor.id,
                     name: monitor.name,
@@ -306,9 +300,11 @@ function renderLargeBlock(entriesType) {
                 });
 
                 chartDataObject['labels'] = getLargeChartLabels(newPeriod, maxLength);
+                chartDataObject['period'] = oldPeriod;
                 renderLargeBlockChart(chartDataObject, entriesType);
-            });
-        }
+            }
+
+        });
     });
 }
 
@@ -337,9 +333,9 @@ function renderLargeBlockChart(monitorsObject, entriesType) {
     var options = null;
     var chart = null;
 
-    var colors = ["#1f8ef1", "#14bda8", "#1f8ef1", "#f11f92", "#1f8ef1", "#f11f92",];
+    var colors = ["#14bda8", "#1f8ef1", "#1f8ef1", "#f11f92", "#1f8ef1", "#f11f92",];
     var barBgColors = ["#1f8ef1", "#f11f92", "transparent", "transparent"];
-    var chartLabels = ["", " (1 day previous)"]
+    var chartLabels = ["", " (1 " + monitorsObject.period + " previous)"]
     var bDash = [[], [15, 4, 4, 4]]
     for (monitor of monitorsObject) {
         switch (entriesType) {
@@ -366,7 +362,8 @@ function renderLargeBlockChart(monitorsObject, entriesType) {
                     stack: i,
                     categoryPercentage: 1.0,
                     barPercentage: 1.0,
-                    barThickness: 70,
+                    barThickness: 50,
+                    maxBarThickness: 70,
                     data: monitor.result.maleData
                 }, {
                     label: 'Female' + chartLabels[j],
@@ -375,6 +372,8 @@ function renderLargeBlockChart(monitorsObject, entriesType) {
                     borderColor: colors[i + 2 + 1],
                     backgroundColor: barBgColors[i + 1],
                     barThickness: 70,
+                    barThickness: 50,
+                    maxBarThickness: 70,
                     borderWidth: 2,
                     stack: i,
                     data: monitor.result.femaleData
@@ -386,8 +385,6 @@ function renderLargeBlockChart(monitorsObject, entriesType) {
         i++;
         j++;
     }
-
-
 
     var options = {
         maintainAspectRatio: false,
@@ -447,8 +444,8 @@ function getDataOfMonitor(entriesType, query) {
 function showAnalyticsSummaryStats(dataObject, period) {
     $('#analytics-page .card:nth-child(1) h2')[0].textContent = 'Last ' + period + ' summary';
 
+    console.log(dataObject)
     var totalSum = getObjectTotalSum(dataObject);
-    var period = 'day';
     if (period == 'day')
         period = 'dai';
 
@@ -476,17 +473,12 @@ function togglePageSelector(entriesType) {
     }
 }
 
-
-function getAnalyticsPageLabes() {
-
-}
-
 function getLargeChartLabels(period, n) {
     var labels = [];
 
     if (period == 'hour') {
         for (var i = 0; i < 24; i++)
-            labels.push( i + ' - ' + (i + 1))
+            labels.push(i + ' - ' + (i + 1))
     } else if (period == 'day') {
         for (var i = 0; i < 7; i++)
             labels.push('Day ' + (i + 1))
@@ -500,3 +492,62 @@ function getLargeChartLabels(period, n) {
     }
     return labels;
 }
+
+
+function getAnalyticPagePeriod(period) {
+    var cur = moment();
+
+    switch (period) {
+        case 'day':
+            end1 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'end');
+            start1 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'start');
+            cur.subtract(1, period);
+
+            start2 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'start');
+            end2 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'end');
+            newPeriod = 'hour';
+            break;
+        case 'week':
+            end1 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'end');
+            cur.subtract(1, period);
+            start1 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'start');
+
+            end2 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'end');
+            cur.subtract(1, period);
+            start2 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'start');
+            newPeriod = 'day';
+            break;
+        case 'month':
+            end1 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'end');
+            cur.subtract(1, period);
+            start1 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'start');
+
+            end2 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'end');
+            cur.subtract(1, period);
+            start2 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'start');
+            newPeriod = 'week';
+            break;
+        case 'year':
+            end1 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'end');
+            cur.subtract(1, period);
+            start1 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'start');
+
+            end2 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'end');
+            cur.subtract(1, period);
+            start2 = dateToTimestamp(cur.format("YYYY-MM-DD"), 'start');
+            newPeriod = 'month';
+            break;
+        default:
+            break;
+    }
+    return { end1, start1, start2, end2, newPeriod };
+}
+
+
+function getRequestsArr(entriesType, queries) {
+    var requestsArr = [];
+    for (query of queries) {
+        requestsArr.push(getDataOfMonitor(entriesType, query))
+    }
+    return requestsArr;
+}    
